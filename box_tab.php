0418a899e6b1c973cd0e3d198063e9edb8805eb5
macbook <hai.lemanh.1310@gmail.com>
2019-05-08 10:42:01 +0700

    update

--- box_tab.php
+++ box_tab.php
@@ -1,7 +1,7 @@
 <div>
     <ul class="uk-child-width-expand uk-margin-remove uk-grid-collapse tab1" uk-tab="connect: #my-id" uk-grid>
-        <li class="uk-active"><a href="#">trò chuyện <span class="uk-badge">12</span></a></li>
-        <li><a href="#">chat bot <span class="uk-badge">12</span></a></li>
+        <li><a href="#">trò chuyện <span class="uk-badge">12</span></a></li>
+        <li class="uk-active"><a href="#">chat bot <span class="uk-badge">12</span></a></li>
     </ul>
     <?php include('top.php'); ?>
 </div>
--- js/functions.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('#scrollToBottom').bind("click", function () {
-        $('.chat').animate({ scrollTop: $(document).height() + 500 }, 1500);
-        return false;
-    });
-});
